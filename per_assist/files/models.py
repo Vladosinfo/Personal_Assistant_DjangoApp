14aff66467eafa5bdd18a0aa31ce3944bef99eec
Vlad <vladyslav.pasko@gmail.com>
2024-05-19 19:35:53 +0300

    Some changes file upload related

--- per_assist/files/models.py
+++ per_assist/files/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 
 class File(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     file = models.FileField(upload_to='files/')
     category = models.CharField(max_length=50)
--- per_assist/files/views.py
+++ per_assist/files/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import FileUploadForm
 from .models import File
 
 
+@login_required
 def upload_file(request):
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
