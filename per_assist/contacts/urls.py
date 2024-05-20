99fe2f8d8ee0b62da64d5cb6ba3dd0882bbf2cff
maksym-sk21 <148781411+maksym-sk21@users.noreply.github.com>
2024-05-20 12:26:19 +0300

    Update urls.py

--- per_assist/contacts/urls.py
+++ per_assist/contacts/urls.py
@@ -1,13 +1,14 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 app_name = 'contacts'
 
 urlpatterns = [
     path('', views.main, name='main'),
-    # path('author/', views.author, name='author'),
-    # path('detail/<int:quote_id>', views.detail, name='detail'),
-    # path('delete/<int:quote_id>', views.delete_quote, name='delete'),
-    # # path('', views.main, name='main'),
-    # # re_path('', views.main, name='main'),
+    path('contacts/', views.contact_book, name='contact_book'),
+    path('birthdays/', views.contacts_with_upcoming_birthdays, name='contacts_with_upcoming_birthdays'),
+    path('search/', views.search_contacts, name='search_contacts'),
+    path('add/', views.add_contact, name='add_contact'),
+    path('edit/<int:pk>/', views.edit_contact, name='edit_contact'),
+    path('delete/<int:pk>/', views.delete_contact, name='delete_contact'),
 ]
