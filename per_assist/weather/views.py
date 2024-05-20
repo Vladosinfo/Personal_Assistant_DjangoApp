cfc028113191de21896e48cc53d6157f945078f8
Oleksandr <132605972+0leksandrZahar0vi4@users.noreply.github.com>
2024-05-21 00:06:20 +0300

    first_commit_weather

---
+++ per_assist/weather/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required
+def weather_list(request):
+
+    return render(request, 'weather/weather_list.html')
