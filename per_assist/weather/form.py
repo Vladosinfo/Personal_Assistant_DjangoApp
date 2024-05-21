8097f04d39554c0ceee0cafd557a0c5d5fd684b0
Oleksandr <132605972+0leksandrZahar0vi4@users.noreply.github.com>
2024-05-21 20:01:04 +0300

    third_commit_model_weather

--- per_assist/weather/form.py
+++ per_assist/weather/form.py
@@ -1,12 +1,11 @@
 from django.forms import ModelForm, CharField, TextInput, TextField, Textarea
-from .models import WeatherNews
+from .models import Weather
 
 
 class WeatherForm(ModelForm):
-    # name = CharField(min_length=3, max_length=25, required=True, widget=TextInput())
     cur_day = CharField(min_length=3, max_length=50, required=True)
     min_temperature = CharField(min_length=3, max_length=50, required=True)
     max_temperature = CharField(min_length=3, max_length=50, required=True)
     class Meta:
-        model = WeatherNews
+        model = Weather
         fields = ['cur_day', 'min_temperature', 'max_temperature']
--- per_assist/weather/views.py
+++ per_assist/weather/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# from ..notes.models import Tag, Note
+from .models import Weather
+from .scrapy import QuotesSpider, DataPipline
 
 # Create your views here.
 @login_required
@@ -9,10 +10,10 @@ def weather_list(request):
     return render(request, 'weather/weather_list.html')
 
 
-# def notes(request, tag_id=None):
-#     if request.GET.get('tag_id') != None:
-#         tag = Tag.objects.get(id=request.GET.get('tag_id'))
-#         notes = Note.objects.filter(tags=tag, user=request.user)
-#     else:
-#         notes = Note.objects.filter(user=request.user)
+def weather(request, cur_date=None):
+    # if request.GET.get('cur_date') != None:
+    cur_date = QuotesSpider.objects.get(id=request.GET.get('cur_date'))
+        # notes = Note.objects.filter(tags=tag, user=request.user)
+    # else:
+    #     notes = Weather.objects.filter(user=request.user)
 
