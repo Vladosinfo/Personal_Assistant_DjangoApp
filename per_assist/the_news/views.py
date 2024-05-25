9516ff011a4cb94f6b3f48112da2608600165ad6
ValeraPrus <prusvalerii@gmail.com>
2024-05-25 06:50:27 +0300

    news update

--- per_assist/the_news/views.py
+++ per_assist/the_news/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.management import call_command
+# import subprocess
 from .models import News
 
 
 def news_list(request):
     news = News.objects.all()
-    return render(request, 'news/news_list.html', {'news': news})
+    selected_category = request.GET.get('categories')
+
+    if selected_category and selected_category != '':
+        news = news.filter(category__contains=selected_category)
+    return render(request, 'news/news_list.html', {'news': news, 'selected_category': selected_category})
 
 
 def news_detail(request, pk):
@@ -12,6 +18,14 @@ def news_detail(request, pk):
     return render(request, 'news/news_detail.html', {'news': news})
 
 
-def news_category(request, category):
-    news = News.objects.filter(category=category)
-    return render(request, 'news/news_list.html', {'news': news})
+# def news_category(request, category):
+#     news = News.objects.filter(category=category)
+#     return render(request, 'news/news_list.html', {'news': news})
+
+
+# def fetch_news():
+#     subprocess.Popen(['python', 'manage.py', 'fetch_news'])
+
+def fetch_news_view(request):
+    call_command('fetch_news')
+    return redirect('the_news:news_list')
