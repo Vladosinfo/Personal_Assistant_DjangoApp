d878f9a9df98da794a25f75ea3e10cef75001d44
maksym-sk21 <148781411+maksym-sk21@users.noreply.github.com>
2024-05-25 08:58:23 +0300

    Update urls.py

--- per_assist/contacts/urls.py
+++ per_assist/contacts/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('add/', views.add_contact, name='add_contact'),
     path('edit/<int:pk>/', views.edit_contact, name='edit_contact'),
     path('delete/<int:pk>/', views.delete_contact, name='delete_contact'),
+    path('contact/<int:pk>/', views.contact_detail, name='contact_detail'),
 ]
