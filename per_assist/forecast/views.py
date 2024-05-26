ddf20ccbc016dda5e64a4ed6b28b344c33dfb243
ValeraPrus <prusvalerii@gmail.com>
2024-05-27 02:05:59 +0300

    weather update

--- per_assist/forecast/views.py
+++ per_assist/forecast/views.py
@@ -1,9 +1,10 @@
 import requests
 from datetime import datetime
 from django.shortcuts import render
-from django.conf import settings
+# from django.conf import settings
 # from django.http import JsonResponse
 from .models import Weather
+from .dict_folder.dictionaryes import weather_descriptions, location_description
 
 
 # def get_weather(request):
@@ -25,42 +26,10 @@ from .models import Weather
 #     return render(request, 'weather_forecast.html', {'weather': weather})
 
 
-weather_descriptions = {
-    0: 'Clear sky',
-    1: 'Mainly clear',
-    2: 'Partly cloudy',
-    3: 'Overcast',
-    45: 'Fog',
-    48: 'Depositing rime fog',
-    51: 'Light drizzle',
-    53: 'Moderate drizzle',
-    55: 'Dense drizzle',
-    56: 'Light freezing drizzle',
-    57: 'Dense freezing drizzle',
-    61: 'Slight rain',
-    63: 'Moderate rain',
-    65: 'Heavy rain',
-    66: 'Light freezing rain',
-    67: 'Heavy freezing rain',
-    71: 'Slight snow fall',
-    73: 'Moderate snow fall',
-    75: 'Heavy snow fall',
-    77: 'Snow grains',
-    80: 'Slight rain showers',
-    81: 'Moderate rain showers',
-    82: 'Violent rain showers',
-    85: 'Slight snow showers',
-    86: 'Heavy snow showers',
-    95: 'Thunderstorm',
-    96: 'Thunderstorm with slight hail',
-    99: 'Thunderstorm with heavy hail'
-}
-
-
 def get_weather_forecast(request):
-    latitude = request.GET.get('latitude', '50.4501')
-    longitude = request.GET.get('longitude', '30.5234')
-    url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,precipitation_sum,weathercode&timezone=Europe/Kiev'
+    loc = request.GET.get('location', '50.4501,30.5234')
+    latitude, longitude = loc.split(',')
+    url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,precipitation_sum,/weathercode&timezone=Europe/Kiev'
 
     response = requests.get(url)
     data = response.json()
@@ -73,9 +42,10 @@ def get_weather_forecast(request):
         max_temp = forecast['temperature_2m_max'][i]
         weather_code = forecast['weathercode'][i]
         description = weather_descriptions.get(weather_code, 'Unknown')
+        location = location_description.get(loc, 'Kyiv')
 
         weather = Weather(
-            location='Kyiv',
+            location=location,
             temperature=max_temp,
             description=description,
             forecast_date=date
@@ -83,12 +53,12 @@ def get_weather_forecast(request):
         weather.save()
         weather_forecast.append(weather)
 
-    return render(request, 'weather_forecast.html', {'weather_forecast': weather_forecast})
+    return render(request, 'weather_forecast.html', {'weather_forecast': weather_forecast, 'location': location})
 
 
 def get_current_weather(request):
-    latitude = request.GET.get('latitude', '50.4501')
-    longitude = request.GET.get('longitude', '30.5234')
+    loc = request.GET.get('location', '50.4501,30.5234')
+    latitude, longitude = loc.split(',')
     url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true'
 
     response = requests.get(url)
@@ -98,13 +68,14 @@ def get_current_weather(request):
     temperature = current_weather.get('temperature', 0)
     weather_code = current_weather.get('weathercode', 0)
     description = weather_descriptions.get(weather_code, 'Unknown')
+    location = location_description.get(loc, 'Kyiv')
 
     weather = Weather(
-        location='Kyiv',
+        location=location,
         temperature=temperature,
         description=description,
         forecast_date=datetime.today().date()
     )
     weather.save()
 
-    return render(request, 'current_weather.html', {'weather': weather})
+    return render(request, 'current_weather.html', {'weather': weather, 'location': location})
