a1b29f74b43a3f5f221daf694792ddd9dc9fd97a
Vlad <vladyslav.pasko@gmail.com>
2024-05-29 06:40:41 +0300

    Fix main page and contacts filter by authorised user.

--- per_assist/contacts/views.py
+++ per_assist/contacts/views.py
@@ -8,24 +8,28 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def main(request):
 
-    contact_list = Contact.objects.all().order_by('-id')
+    if request.user.is_authenticated:
 
-    # Pagination
-    paginator = Paginator(contact_list, 10)
-    page_number = request.GET.get('page')
-    try:
-        contact_list = paginator.page(page_number)
-    except PageNotAnInteger:
-        contact_list = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        contact_list = paginator.page(paginator.num_pages)
+        contact_list = Contact.objects.filter(user=request.user).order_by('-id')
+
+        # Pagination
+        paginator = Paginator(contact_list, 10)
+        page_number = request.GET.get('page')
+        try:
+            contact_list = paginator.page(page_number)
+        except PageNotAnInteger:
+            contact_list = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            contact_list = paginator.page(paginator.num_pages)
+    else:
+        contact_list = []
    
     return render(request, 'contacts/index.html', {"contact_list": contact_list})   
 
 
 def contact_book(request):
-    contact_list = Contact.objects.all().order_by('-id')
+    contact_list = Contact.objects.filter(user=request.user).order_by('-id')
 
     search_form = ContactSearchForm(request.GET or None)
     if 'find_contact_criteria' in request.GET and 'find_contact_value' in request.GET:
@@ -34,25 +38,25 @@ def contact_book(request):
             search_criteria = search_form.cleaned_data['find_contact_criteria']
             search_value = search_form.cleaned_data['find_contact_value']
             if search_criteria == 'name':
-                contact_list = contact_list.filter(
+                contact_list = contact_list.filter(user=request.user, 
                     name__icontains=search_value).order_by('-id')
             elif search_criteria == 'surname':
-                contact_list = contact_list.filter(
+                contact_list = contact_list.filter(user=request.user, 
                     surname__icontains=search_value).order_by('-id')
             elif search_criteria == 'phone':
-                contact_list = contact_list.filter(
+                contact_list = contact_list.filter(user=request.user, 
                     phone__icontains=search_value).order_by('-id')
             elif search_criteria == 'email':
-                contact_list = contact_list.filter(
+                contact_list = contact_list.filter(user=request.user, 
                     email__icontains=search_value).order_by('-id')
 
     days_ahead_form = DaysAheadForm(request.GET or None)
-    upcoming_birthdays = get_upcoming_birthdays(7)
+    upcoming_birthdays = get_upcoming_birthdays(request, 7)
     if 'days_ahead' in request.GET:
         days_ahead_form = DaysAheadForm(request.GET)
         if days_ahead_form.is_valid():
             days_ahead = days_ahead_form.cleaned_data['days_ahead']
-            contact_list = contact_list.filter(id__in=[contact.id for contact in get_upcoming_birthdays(days_ahead)]).order_by('-id')
+            contact_list = contact_list.filter(id__in=[contact.id for contact in get_upcoming_birthdays(request, days_ahead)]).order_by('-id')
 
     paginator = Paginator(contact_list, 10)
     page_number = request.GET.get('page')
@@ -112,14 +116,14 @@ def delete_contact(request, pk):
                   {'contact': contact})
 
 
-def get_upcoming_birthdays(days_ahead):
+def get_upcoming_birthdays(request, days_ahead):
     if not days_ahead:
         days_ahead = 364
     try:
         days_ahead = int(days_ahead)
     except ValueError:
         days_ahead = 7
-    upcoming_birthdays = Contact.objects.get_birthdays_in_days(days_ahead)
+    upcoming_birthdays = Contact.objects.get_birthdays_in_days(days_ahead).filter(user=request.user)
     return upcoming_birthdays
 
 
